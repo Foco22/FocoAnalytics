3c814884aca450c5bf5cce9fe8b4b71421bfff88
foco22 <francisco.macaya22@gmail.com>
2023-04-29 02:20:55 -0400

    first commit

---
+++ App.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from markupsafe import Markup
+from flask import render_template
+
+app = Flask(__name__,template_folder='template/')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/first.html')
+def firsr():
+    return render_template('first.html')
+    
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port=8000)
