2bfcd3ea02da560501cecdf1a8282084789bc19d
foco22 <francisco.macaya22@gmail.com>
2024-04-05 18:07:29 -0300

    fix

--- App.py
+++ App.py
@@ -4,14 +4,19 @@ from flask import render_template
 
 app = Flask(__name__,template_folder='template/')
 
-@app.route('/')
+@app.route('/') 
 def index():
     return render_template('index.html')
 
 @app.route('/first.html')
-def firsr():
+def first():
     return render_template('first.html')
-    
 
+@app.route('/second.html')
+def second():
+    return render_template('second.html')
+    
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=8000)
+
+    
