bd80c6ffa608538c5892bf40766f85107cc46778
foco22 <francisco.macaya22@gmail.com>
2024-04-10 21:29:35 -0400

    fix

--- static/script.js
+++ static/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Data Science','Machine Learning','Inteligencia Artificial','Programacion'],
+    strings: ['Data Science','Machine Learning','Inteligencia Artificial','Programación'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
