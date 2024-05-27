c312fb3c2d33c23d5da15265894965821b3410ff
foco22 <francisco.macaya22@gmail.com>
2024-05-26 22:21:48 -0400

    fix

--- App.py
+++ App.py
@@ -15,6 +15,10 @@ def first():
 @app.route('/second.html')
 def second():
     return render_template('second.html')
+
+@app.route('/third.html')
+def third():
+    return render_template('third.html')
     
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=8000)
